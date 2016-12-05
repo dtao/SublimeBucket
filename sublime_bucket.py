e10957520738cbedcd99bb99efffc51b332eeb26
Dan Tao <daniel.tao@gmail.com>
2016-12-05 12:40:36 -0600

    pulled subprocess.check_output w/ common args into _exec helper method

--- sublime_bucket.py
+++ sublime_bucket.py
@@ -8,18 +8,20 @@ BITBUCKET_HOST = 'bitbucket.org'
 
 class OpenInBitbucketCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        full_path = self.view.file_name()
+        self.full_path = self.view.file_name()
+        self.directory = os.path.dirname(self.full_path)
+
         url = 'https://%(host)s/%(repo)s/src/%(branch)s/%(path)s#%(hash)s' % {
             'host': BITBUCKET_HOST,
-            'repo': self.find_bitbucket_repo(full_path),
-            'branch': self.find_current_revision(full_path),
-            'path': self.get_file_path(full_path),
-            'hash': '%s-%s' % (os.path.basename(full_path),
+            'repo': self.find_bitbucket_repo(),
+            'branch': self.find_current_revision(),
+            'path': self.get_file_path(),
+            'hash': '%s-%s' % (os.path.basename(self.full_path),
                                ','.join(self.get_line_ranges()))
         }
         subprocess.call(['open', url])
 
-    def find_bitbucket_repo(self, full_path):
+    def find_bitbucket_repo(self):
         """Get the Bitbucket repo (username/repo_slug) for the current file.
 
         This method works by invoking `git remote -v` and inspecting the output
@@ -27,13 +29,9 @@ class OpenInBitbucketCommand(sublime_plugin.TextCommand):
         again with `hg paths` for Mercurial repositories.
         """
         try:
-            remotes = subprocess.check_output('git remote -v',
-                                              cwd=os.path.dirname(full_path),
-                                              shell=True).splitlines()
+            remotes = self._exec('git remote -v').splitlines()
         except subprocess.CalledProcessError:
-            remotes = subprocess.check_output('hg paths',
-                                              cwd=os.path.dirname(full_path),
-                                              shell=True).splitlines()
+            remotes = self._exec('hg paths').splitlines()
 
         bitbucket_pattern = (r'%s[:/]([\w\-]+)/([\w\-]+)(?:\.git)?' %
                              BITBUCKET_HOST)
@@ -42,7 +40,7 @@ class OpenInBitbucketCommand(sublime_plugin.TextCommand):
             if remote_match:
                 return '%s/%s' % remote_match.groups()
 
-    def find_current_revision(self, full_path):
+    def find_current_revision(self):
         """Get the hash of the commit/changeset that's currently checked out.
 
         This method works by invoking `git show HEAD` and parsing the output.
@@ -50,26 +48,22 @@ class OpenInBitbucketCommand(sublime_plugin.TextCommand):
         repositories.
         """
         try:
-            info = subprocess.check_output('git show HEAD',
-                                           cwd=os.path.dirname(full_path),
-                                           shell=True).splitlines()
+            info = self._exec('git show HEAD').splitlines()
             for line in info:
                 revision_match = re.search(r'commit (\w+)', str(line))
                 if revision_match:
                     return revision_match.group(1)
         except subprocess.CalledProcessError:
-            revision = subprocess.check_output('hg id -i',
-                                               cwd=os.path.dirname(full_path),
-                                               shell=True).splitlines()[0]
-            return revision.decode('utf-8')
+            revision = self._exec('hg id -i')
+            return revision.decode('utf-8').strip()
 
-    def get_file_path(self, full_path):
+    def get_file_path(self):
         """Get the path to the current file, relative to the repository root.
         """
         folders = self.view.window().folders()
         for folder in folders:
-            if full_path.startswith(folder):
-                file_path = full_path[len(folder):]
+            if self.full_path.startswith(folder):
+                file_path = self.full_path[len(folder):]
                 if file_path.startswith('/'):
                     file_path = file_path[1:]
                 return file_path
@@ -91,3 +85,8 @@ class OpenInBitbucketCommand(sublime_plugin.TextCommand):
                                          max(first_line, last_line)))
 
         return ranges
+
+    def _exec(self, command):
+        """Execute command with cwd set to the project path and shell=True.
+        """
+        return subprocess.check_output(command, cwd=self.directory, shell=True)
