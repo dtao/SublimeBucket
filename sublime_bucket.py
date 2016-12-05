cbd8403d8536749af92eb3cf71764584236d79ef
Dan Tao <daniel.tao@gmail.com>
2016-12-05 12:02:59 -0600

    added logic to detect current (Git) revision

--- sublime_bucket.py
+++ sublime_bucket.py
@@ -9,9 +9,10 @@ BITBUCKET_HOST = 'bitbucket.org'
 class OpenInBitbucketCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         full_path = self.view.file_name()
-        url = 'https://%(host)s/%(repo)s/src/HEAD/%(path)s#%(hash)s' % {
+        url = 'https://%(host)s/%(repo)s/src/%(branch)s/%(path)s#%(hash)s' % {
             'host': BITBUCKET_HOST,
             'repo': self.find_bitbucket_repo(full_path),
+            'branch': self.find_current_revision(full_path),
             'path': self.get_file_path(full_path),
             'hash': '%s-%s' % (os.path.basename(full_path),
                                ','.join(self.get_line_ranges()))
@@ -27,12 +28,24 @@ class OpenInBitbucketCommand(sublime_plugin.TextCommand):
         remotes = subprocess.check_output('git remote -v',
                                           cwd=os.path.dirname(full_path),
                                           shell=True).splitlines()
-        bitbucket_pattern = re.compile(r'%s[:/]([\w\-]+)/([\w\-]+)\.git' %
-                                       BITBUCKET_HOST)
+        bitbucket_pattern = r'%s[:/]([\w\-]+)/([\w\-]+)\.git' % BITBUCKET_HOST
         for remote in remotes:
             remote_match = re.search(bitbucket_pattern, str(remote))
             if remote_match:
                 return '%s/%s' % remote_match.groups()
+
+    def find_current_revision(self, full_path):
+        """Get the hash of the commit/changeset that's currently checked out.
+
+        This method works by invoking `git show HEAD` and parsing the output.
+        """
+        info = subprocess.check_output('git show HEAD',
+                                       cwd=os.path.dirname(full_path),
+                                       shell=True).splitlines()
+        for line in info:
+            revision_match = re.search(r'commit (\w+)', str(line))
+            if revision_match:
+                return revision_match.group(1)
 
     def get_file_path(self, full_path):
         """Get the path to the current file, relative to the repository root.
